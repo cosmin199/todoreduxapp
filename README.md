9167d313046e274e28730a82352e686532506acd
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-30 04:32:59 -0700

    044

---
+++ README.md
@@ -0,0 +1,33 @@
+# React redux todo app
+
+Code for React redux todo app YouTube tutorial, made by chris blakely
+
+- project start to finish
+
+## Running the finished code
+
+Clone the repo and run the following:
+
+- cd api
+- npm install
+- npm run server or nodemon
+
+- npm into todoreduxapp
+- npm install
+- npm start
+
+**NOTE:** The API must be running in order for the finished code to work as intended. Seee "Running the API" below
+
+## Running the API
+
+The API is a dead basic node.js/express server. Clone the repo and run the following:
+
+- cd api
+- npm install
+- npm run server
+- check its running by going to browser/postman
+
+```
+localhost:7000/todos
+
+```
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -11,6 +11,22 @@ export const getTodosAsync = createAsyncThunk(
   }
 )
 
+export const addTodoAsync = createAsyncThunk(
+  "todos/addTodoAsync",
+  async (payload) => {
+    const response = await fetch("http://localhost:7000/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: payload.title }),
+    })
+
+    if (response.ok) {
+      const todo = await response.json()
+      return { todo }
+    }
+  }
+)
+
 const todoSlice = createSlice({
   name: "todos",
   initialState: [
@@ -42,6 +58,9 @@ const todoSlice = createSlice({
     [getTodosAsync.fulfilled]: (state, action) => {
       console.log("fetched data successfully!")
       return action.payload.todos
+    },
+    [addTodoAsync.fulfilled]: (state, action) => {
+      state.push(action.payload.todo)
     },
   },
 })
